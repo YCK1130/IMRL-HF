54d71118d90124c841410b6cc0a2c65bccc9b869
chenjoachim <lllion66666@gmail.com>
2023-12-22 21:27:34 +0800

    Add bash

---
+++ gymnasium/bash1.sh
@@ -0,0 +1,4 @@
+git reset 5e2c61ab --hard
+python DAC2.py -t DAC
+git reset 0081f5db --hard
+python DAC2.py -t DAC
---
+++ gymnasium/bash2.sh
@@ -0,0 +1,4 @@
+git reset 82cd435b --hard
+python DAC2.py -t DAC
+git reset 2f0a15e3 --hard
+python DAC2.py -t DAC
